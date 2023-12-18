5f64628f4362785137412e53c297fd1bd32644f2
jeel narola <narolajeel29@gmail.com>
2023-12-18 10:12:18 +0530

    Cart Paeg Design

--- Model/save.schema.js
+++ Model/save.schema.js
@@ -3,6 +3,7 @@ const mongoose=require("mongoose")
 const save=new mongoose.Schema({
     productId:{type:mongoose.Schema.Types.ObjectId,ref:"product"},
     userId:{type:mongoose.Schema.Types.ObjectId,ref:"user"},
+    icon:String,
 })
 
 const savemodel=mongoose.model("save",save)
